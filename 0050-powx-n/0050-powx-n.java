841a3aad10a0a51eeee96a5221f2f20c3f51351b
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-06-03 23:14:28 +0530

    Time: 0 ms (100.00%), Space: 41 MB (58.66%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    
+    public double myPow( double x , int n ) 
+    {
+        
+        
+        double pow = Math.pow( x , n );
+        
+        return pow;
+        
+    }
+}
+
