1d0fa373295014fd27cbdf29b718cf45be7da399
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-06-10 13:38:51 +0530

    Time: 1 ms (100.00%), Space: 60 MB (5.11%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    
+    public int maxSubArray(int[] nums) 
+    {
+        // Kande's Algorithm:
+        
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            currSum = Math.max(( currSum + nums[ i ] ), nums[ i ] );
+            maxSum = Math.max( maxSum, currSum );
+        }
+        
+        return maxSum;
+        
+    }
+}
+
