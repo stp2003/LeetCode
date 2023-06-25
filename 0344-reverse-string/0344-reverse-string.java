3161fac8f518e59162515b27ac4d10089747111b
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-06-25 14:28:57 +0530

    Time: 2 ms (14.27%), Space: 51.1 MB (22.93%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        stringBuilder.append(s);
+        
+        stringBuilder.reverse();
+        
+        for (int i = 0; i < s.length; i++) {
+            s[i] = stringBuilder.charAt(i);
+        }
+        
+    }
+}
+
