9d4b1410b4f5f443181780f95bab13422417b5a0
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-10-04 18:37:41 +0530

    Time: 21 ms (12.28%), Space: 43.7 MB (24.34%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,14 @@
+import java.math.*;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        
+        BigInteger a = new BigInteger(num1); 
+        BigInteger b = new BigInteger(num2);
+        
+        BigInteger c = a.add(b);
+        
+        return c.toString(10);   // for base 10.
+        
+    }
+}
