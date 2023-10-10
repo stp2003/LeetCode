e6425eaa1553623a033745c130b41acdb8a8bf8f
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-10-10 13:33:05 +0530

    Time: 0 ms (100.00%), Space: 39 MB (55.33%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -3,16 +3,9 @@ class Solution {
     public boolean canWinNim(int n) {
         
         if(n < 4)
-           return true;
-        
-        if(n > 3) {
-            if(n % 4 == 0)
-                return false;
-            else
-               return true;
-        }
-        
-        return false;
+            return true;
+
+        return n % 4 != 0;
         
     }
 }
