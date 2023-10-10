306813f5152b524e1d809c9563d37344802c8688
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-10-10 15:30:02 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (72.28%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+        
+        if(n <= 2) {
+            return n;
+        }
+        
+        int x = 1, y = 2;
+        for(int i = 2; i < n; i++) {
+            int steps = x;
+            x = y;
+            y = steps + x;
+        }
+        
+        return y;
+        
+    }
+}
+
