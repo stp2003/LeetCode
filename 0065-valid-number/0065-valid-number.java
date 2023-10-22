5575bf0a559ff9df0f42dcabb6e4a4ec6cb2ff10
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-10-22 12:53:02 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (30.01%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,61 @@
+class Solution {
+    
+    public boolean isNumber(String s) {
+        
+        boolean digitSeen = false, eSeen = false, dotSeen = false;
+        int countPlusMinus = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            //** digit
+            if (Character.isDigit(ch)) {
+                digitSeen = true;
+            }
+            
+            //** minus/plus
+            else if (ch == '+' || ch == '-') {
+                if (countPlusMinus == 2) {
+                    return false;
+                }
+
+                if (i > 0 && (s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')) {
+                    return false;
+                }
+
+                if (i == s.length() - 1) {
+                    return false;
+                }
+
+                countPlusMinus++;
+            }
+            
+            //** dot
+            else if (ch == '.') {
+                if (eSeen || dotSeen) {
+                    return false;
+                }
+
+                if (i == s.length() - 1 && !digitSeen) {
+                    return false;
+                }
+
+                dotSeen = true;
+            }
+
+            //** e/E
+            else if (ch == 'e' || ch == 'E') {
+                if (eSeen || !digitSeen || i == s.length() - 1) {
+                    return false;
+                }
+                
+                eSeen = true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+}
