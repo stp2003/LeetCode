366bb746ef835738995d28fbabd0348bb644064c
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-10-27 10:04:52 +0530

    Time: 1 ms (83.13%), Space: 40 MB (12.12%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,17 +3,20 @@ class Solution {
     public boolean isHappy(int n) {
         
         int sum = 0;
-        
-       while (n != 1 && n != 4) {
-        char[] digits = String.valueOf(n).toCharArray();
-        sum = 0;
-        for (char digitChar : digits) {
-            int digit = digitChar - '0';
-            sum += digit * digit;
+
+        while (n != 1 && n != 4) {
+            
+            char[] digits = String.valueOf(n).toCharArray();
+            sum = 0;
+            
+            for (char digitChar : digits) {
+                int digit = digitChar - '0';
+                sum += digit * digit;
+            }
+            
+            n = sum;
         }
-        n = sum;
-    }
-    
-    return n == 1;
+
+        return n == 1;
     }
 }
