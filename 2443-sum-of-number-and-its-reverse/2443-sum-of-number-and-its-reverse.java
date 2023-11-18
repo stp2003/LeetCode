8ed638b7cc08cf8a8a3d968023fed90313d2e78d
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-11-18 10:14:30 +0530

    Time: 147 ms (71.92%), Space: 39.5 MB (52.71%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public boolean sumOfNumberAndReverse(int num) {
+        
+        for (int i = 0; i <= num; i++) {
+            int rev = 0;
+            int temp = i;
+            
+            while (temp != 0) {
+                int r = temp % 10;
+                rev = rev * 10 + r;
+                temp = temp / 10;
+            }
+            
+            if ((i + rev) == num) {
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+}
