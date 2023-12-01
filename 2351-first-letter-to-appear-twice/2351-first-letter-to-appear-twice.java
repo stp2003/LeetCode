af30ca256f120cd7b9642282c0faa121f2d21cc0
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-12-01 09:17:19 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (9.95%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public char repeatedCharacter(String s) {
+        
+        for (int i = 1; i < s.length(); i++) {
+            
+            if (s.indexOf(s.charAt(i)) != i) {
+                
+                return s.charAt(i);
+            }
+        }
+        
+        return ' ';
+        
+    }
+}
