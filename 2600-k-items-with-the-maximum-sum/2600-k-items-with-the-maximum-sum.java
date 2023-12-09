37fc5fcf8a57c8d9aea1e79b3d3f1011aa5a84ed
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-12-09 07:55:59 +0530

    Time: 1 ms (99.75%), Space: 40.5 MB (8.84%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -0,0 +1,8 @@
+class Solution {
+    
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        
+        return Math.min(k, numOnes) - Math.max(0, k - numOnes - numZeros);
+        
+    }
+}
