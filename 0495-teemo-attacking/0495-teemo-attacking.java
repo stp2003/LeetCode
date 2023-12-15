48ecb6af89ef66544b76ce549edbaec0511fc089
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-12-15 08:50:35 +0530

    Time: 3 ms (44.96%), Space: 45.1 MB (25.88%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        
+        if (timeSeries.length == 0) 
+            return 0;
+        
+        int totalTime = 0;
+        
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            
+            totalTime += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        }
+        
+        return totalTime + duration;
+        
+    }
+}
