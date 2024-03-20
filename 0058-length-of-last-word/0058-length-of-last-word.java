ef761903e762af230ec2529d1ecdbb8f537bcc20
Shashwat <100586951+stp2003@users.noreply.github.com>
2024-03-20 14:16:11 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (78.16%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,11 +1,9 @@
 class Solution {
+
     public int lengthOfLastWord(String s) {
-        
         s = s.trim();
         int lastWordIndex = s.lastIndexOf(' ') + 1;
 
-        return ( s.length() - lastWordIndex );  
-
+        return (s.length() - lastWordIndex);
     }
 }
-
