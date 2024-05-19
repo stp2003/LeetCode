3669b8abe27edd7652b570a401e96868d6514202
Shashwat <100586951+stp2003@users.noreply.github.com>
2024-05-19 10:08:07 +0530

    Time: 3 ms (75.08%), Space: 43.2 MB (71.60%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public String sortString(String s) {
+        
+        int[] arr = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            int index = s.charAt(i)-'a';
+            arr[index] = arr[index]+1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(sb.length() < s.length()){
+        
+        for(int i=0; i<26; i++){
+            if (arr[i]>0){
+                char ch = (char)('a'+i);
+                sb.append(ch);
+                arr[i]--;
+            }
+        }
+
+        for(int i=25; i>=0; i--){
+            if (arr[i]>0){
+                char ch = (char)('a'+i);
+                sb.append(ch);
+                arr[i]--;
+            }
+        }
+
+        }
+
+        return sb.toString();
+        
+    }
+}
